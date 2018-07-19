799225145ba9d61beb6a539ac66e8a3dad955071
jahiiir315 <geminis31_98@hotmail.com>
2018-07-19 04:13:05 -0500

    Revert "Revert "co""
    This reverts commit f8a812b359cdb5e8a0ab7a233f4ab17101026304.

--- CasoJsf/src/dto/Grupo.java
+++ CasoJsf/src/dto/Grupo.java
@@ -22,6 +22,9 @@ public class Grupo implements Serializable{
 	@Column(name = "nombre")
 	private String nombre;
 	
+	@Column(name = "descripcion")
+	private String descripcion;
+	
 	
 	public Long getId() {
 		return id;
@@ -31,10 +34,19 @@ public class Grupo implements Serializable{
 		this.id = id;
 	}
 
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}}
+	}
+}
