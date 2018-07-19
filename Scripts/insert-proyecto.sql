72f5060916bc5d9c5d01cfae718da5a7e70f7ba5
crismnv <crisycochea@gmail.com>
2018-07-19 17:20:54 -0500

    Se añadio script con datos

---
+++ Scripts/insert-proyecto.sql
@@ -0,0 +1,19 @@
+
+
+
+INSERT INTO `proyecto` (`id`, `nombre`, `ubicacion`) VALUES
+(2, 'Proyecto 1', 'Los Pinos'),
+(3, 'Proyecto 2', 'Las Malvinas');
+(4, 'Proyecto 3', 'Lima');
+(5, 'Proyecto 4', 'Mendoza');
+
+
+INSERT INTO `etapa` (`id`, `denominacion`, `duracion`, `Proyecto_id`) VALUES
+(4, 'Etapa1', 132, 2),
+(5, 'Etapa2', 12, 2),
+(6, 'Etapa3', 162, 2),
+(7, 'Etapa4', 65, 2),
+(8, 'Etapa1', 151, 3);
+(9, 'Etapa1', 155, 4);
+(10, 'Etapa2', 125, 4);
+(11, 'Etapa1', 156, 5);
